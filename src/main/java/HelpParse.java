51d7a3ec9e07753c49f1cea43d7a753915bbf697
Maxim Yenalyev <maxim.yenalyev@gmail.com>
2017-11-21 02:18:47 +0200

    fixed parseTableToMap method in HelpParse class

--- src/main/java/HelpParse.java
+++ src/main/java/HelpParse.java
@@ -3,43 +3,51 @@ import org.jsoup.select.Elements;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
 
 public class HelpParse {
     // Таблиця розбивається на блоки по два стобці, в кожному блоці перший стовбець - стовбець заголовка, другий - значень
     // If row of title has duplicate values and different values in second row then their values concatinated
-    public HashMap<String, String> parseTableToMap(Element table, boolean includeFirstRaw){
+    public static HashMap<String, String> parseTableToMap(Element table){
         HashMap <String, String> resultMap = new HashMap<String, String>();
         Elements rows = table.select("tr");
-        Elements colKey;
-        Elements colValue;
+        Elements cols = rows.get(0).select("td");
+        Elements colKey = null;
+        //Elements colValue;
         ArrayList<String> keys = new ArrayList<String>();
         ArrayList <String> values = new ArrayList<String>();
-        int countPairRows = rows.size() / 2;
-        for (int i = 0; i < countPairRows; i++){
-            colKey = rows.get(i).select("td");
-            for (int k = 0; k < colKey.size(); k++){
-                keys.add(colKey.get(k).text());
-            }
-            colValue = rows.get(i + 1).select("td");
-            for (int k = 0; k < colValue.size(); k++){
-                values.add(colKey.get(k).text());
-            }
-            if (keys.size() != values.size()){
-                resultMap.put("Feedback", "Different list sizes");
-            }
-            for (int k = 0; k < colKey.size(); k++){
-                if (resultMap.containsKey(keys.get(k))){
-                    if (resultMap.get(keys.get(k)).equals(values.get(k))){}
+        String lastColumnValue = "";
+        StringBuilder sb = new StringBuilder();
+        int countPairCol = cols.size() / 2;
+        for(int j = 0; j < rows.size(); j++){
+            for (int i = 0; i < countPairCol; i++){
+                colKey = rows.get(j).select("td");
+                keys.add(colKey.get(2*i).text());
+                //colValue = rows.get(j).select("td");
+                values.add(colKey.get(2*i+1).text());
+
+                if (keys.size() != values.size()){
+                    resultMap.put("Feedback", "Different list sizes");
+                }
+                int lastKeyInList = keys.size()-1;
+                if (resultMap.containsKey(keys.get(lastKeyInList))){
+                    if (resultMap.get(keys.get(lastKeyInList)).equals(values.get(lastKeyInList))){}
                     else {
-                        String newValue = resultMap.get(keys.get(k)) + ", " + values.get(k);
-                        resultMap.put(keys.get(k), newValue);
+                        String newValue = resultMap.get(keys.get(lastKeyInList)) + ", " + values.get(lastKeyInList);
+                        resultMap.put(keys.get(lastKeyInList), newValue);
                     }
                 } else{
-                    resultMap.put(keys.get(k), values.get(k));
+                    resultMap.put(keys.get(lastKeyInList), values.get(lastKeyInList));
                 }
-            }
 
+            }
+            if (cols.size() > 2 * countPairCol){
+
+                sb.append(colKey.get(colKey.size()-1).text() + ", ");
+                lastColumnValue = sb.toString();
+            }
+        }
+        if (cols.size() > 2 * countPairCol){
+            resultMap.put("Last Column", lastColumnValue);
         }
 
         return resultMap;
